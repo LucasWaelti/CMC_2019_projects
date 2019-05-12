9c3df7e13809b0460e93d7f5fbe36ca97fed3381
Lucas Waelti <lucas.waelti@gmail.com>
2019-05-12 21:34:38 +0200

    Implemented 9b, grid search done

--- Lab9/Webots/controllers/pythonController/exercise_9b.py
+++ Lab9/Webots/controllers/pythonController/exercise_9b.py
@@ -1,10 +1,16 @@
 """Exercise 9b"""
 
 import numpy as np
+from matplotlib import pyplot as plt 
 from run_simulation import run_simulation
 from simulation_parameters import SimulationParameters
 import plot_results 
 
+def get_search_space():
+    num_trials = 5
+    amplitudes = np.linspace(0.05, 0.2, num=num_trials) 
+    phase_lags = np.linspace(0.1,2,num=num_trials)  
+    return num_trials,amplitudes,phase_lags 
 
 def exercise_9b(world, timestep, reset):
     """Exercise 9b"""
@@ -14,41 +20,94 @@ def exercise_9b(world, timestep, reset):
             - phase lag
             - amplitude
         on: 
+            - speed (along the x axis)
             - energy (integral of [torque * joint's speed])
 
         -> Run grid search on those parameters. For each run, evaluate: 
-            - minimal energy (absolute value)
             - maximal energy (absolute value)
-            - average energy
         -> Display 2D plot of results of the grid search. 
     """
     # Parameters
     n_joints = 10
-    amplitudes = np.arange(0.01, 0.1, 0.05) 
-    phase_lags = np.arange(0.1,1,0.1) 
-    parameter_set = [SimulationParameters(
-        drive=drive,
-        amplitude=[0.08,0.2], # head, tail 
-        phase_lag=2*np.pi/10,
-        turn=None,
-        couplingBody=10, 
-        couplingLeg=30,
-        rate=20,
-        cRBody = [0.025, 0.005],
-        simulation_duration = 1
-        )
-        for drive in [2.5,5] # Implement walking and swimming 
-    ]
 
-    # Grid search
-    for simulation_i, parameters in enumerate(parameter_set):
-        reset.reset()
-        run_simulation(
-            world,
-            parameters,
-            timestep,
-            int(1000*parameters.simulation_duration/timestep),
-            logs="./logs/example/simulation_{}.npz".format(simulation_i)
-        )
-        plot_results.plot_simulation(plot=True, path="./logs/example/simulation_{}.npz".format(simulation_i)) 
+    # Search space definition
+    num_trials,amplitudes,phase_lags = get_search_space()
+
+    # Grid search 
+    simulation_i = -1
+    for amplitude in amplitudes:
+        for phase_lag in phase_lags:
+            simulation_i += 1
+            parameters = SimulationParameters(
+                drive=5,
+                amplitude=[amplitude,amplitude], # head, tail 
+                phase_lag=phase_lag,
+                turn=None,
+                couplingBody=10, 
+                couplingLeg=30,
+                rate=20,
+                cRBody = [0.025, 0.005],
+                simulation_duration = 10
+            )
+
+            reset.reset()
+            run_simulation(
+                world,
+                parameters,
+                timestep,
+                int(1000*parameters.simulation_duration/timestep),
+                logs="./logs/9b/simulation_{}.npz".format(simulation_i)
+            )
+    
+    # Evaluate the grid search 
+    compute_energy_speed() 
+
+def compute_energy_speed():
+    n_joints = 10
+
+    _,amplitudes,phase_lags = get_search_space() 
+
+    max_energy = []
+    max_speed = []
+    energy_labels = ['amplitude','phase_lag','energy']
+    speed_labels = ['amplitude','phase_lag','speed']
+
+    simulation_i = -1
+    for amplitude in amplitudes:
+        for phase_lag in phase_lags:
+            simulation_i += 1
+            # Load data
+            with np.load("./logs/9b/simulation_{}.npz".format(simulation_i)) as data:
+                # Load stored data
+                timestep = float(data["timestep"])
+                link_data = data["links"][:, 0, :] # Only get the head position 
+                joints = data["joints"] # (position, velocity, command, torque, torque_fb, output)
+
+                # Compute energy derivative
+                d_energy = np.array([joints[:,i,1]*joints[:,i,4] for i in range(n_joints)]).transpose() # shape (250,10) 
+                # Integrate the energy
+                energy = np.zeros_like(d_energy)
+                for j in range(d_energy.shape[0]):
+                    energy[j] = np.abs(np.sum(d_energy[0:j,:],axis=0))
+                # Return the maximal energy 
+                max_energy.append([amplitude,phase_lag,np.max(np.max(energy))])
+
+                # Compute the speed
+                speed = np.array([(link_data[i+1,:]-link_data[i,:])/timestep for i in range(link_data.shape[0]-1)]) 
+                max_speed.append([amplitude,phase_lag,np.max(speed[:,0],axis=0)])
+
+    max_energy = np.array(max_energy)
+    plt.figure()
+    plot_results.plot_2d(max_energy,energy_labels,n_data=len(max_energy))
+    plt.savefig("./logs/9b/energy.jpg") 
+
+    max_speed = np.array(max_speed)
+    plt.figure()
+    plot_results.plot_2d(max_speed,speed_labels,n_data=len(max_speed))
+    plt.savefig("./logs/9b/speed.jpg") 
+
+    plt.show() 
+
+if __name__ == "__main__":
+    compute_energy_speed()
 
