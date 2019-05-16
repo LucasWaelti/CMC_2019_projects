Corrected speed computation using norm on x and z

Lab9/Webots/controllers/pythonController/exercise_9b.py
@@ -97,8 +97,9 @@ def compute_energy_speed():
 
                 # Compute the speed
                 #speed = np.array([(link_data[i+1,:]-link_data[i,:])/timestep for i in range(link_data.shape[0]-1)]) 
-                speed = (link_data[-1,0] - link_data[0,0])/sim_time
-                max_speed.append([amplitude,phase_lag,speed]) # np.max(speed[:,0],axis=0)
+                speed_x = (link_data[-1,0] - link_data[0,0])/sim_time
+                speed_z = np.abs((link_data[-1,2] - link_data[0,2])/sim_time)
+                max_speed.append([amplitude,phase_lag,np.sqrt(speed_x**2+speed_z**2)]) # np.max(speed[:,0],axis=0)
 
     max_energy = np.array(max_energy)
     plt.figure()